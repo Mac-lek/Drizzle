d0937a80a53f686a38013685ed193fe9ee44e7bd
Macaulay Olalekan <iconmaclek100@gmail.com>
2026-05-20 10:35:11 +0100

    feat(notifications): revert to nodemailer, remove resend

--- src/notifications/providers/nodemailer.provider.ts
+++ src/notifications/providers/nodemailer.provider.ts
@@ -1,29 +1,27 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { Resend } from "resend";
+import * as nodemailer from "nodemailer";
 
 @Injectable()
 export class EmailProvider {
   private readonly logger = new Logger(EmailProvider.name);
-  private readonly resend: Resend;
+  private readonly transporter: nodemailer.Transporter;
   private readonly from: string;
 
   constructor(private readonly config: ConfigService) {
-    this.resend = new Resend(this.config.getOrThrow<string>("RESEND_API_KEY"));
+    this.transporter = nodemailer.createTransport({
+      host: this.config.getOrThrow<string>("MAIL_HOST"),
+      port: this.config.getOrThrow<number>("MAIL_PORT"),
+      secure: this.config.get<number>("MAIL_PORT") === 465,
+      auth: {
+        user: this.config.getOrThrow<string>("MAIL_USER"),
+        pass: this.config.getOrThrow<string>("MAIL_PASS"),
+      },
+    });
     this.from = this.config.getOrThrow<string>("MAIL_FROM");
   }
 
   async sendEmail(to: string, subject: string, body: string): Promise<void> {
-    const { error } = await this.resend.emails.send({
-      from: this.from,
-      to,
-      subject,
-      html: body,
-    });
-
-    if (error) {
-      this.logger.error({ error, to }, "Email delivery failed");
-      throw new Error(error.message);
-    }
+    await this.transporter.sendMail({ from: this.from, to, subject, html: body });
   }
 }
